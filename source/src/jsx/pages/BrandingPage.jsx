ba1111fd9634eff83340edce021d6534a6f10df3
max <70626784+notmax6677@users.noreply.github.com>
2022-06-09 20:48:12 +0400

    Added news page, fixed some css
    News page has JSON post support.
    - Also added donation page but haven't started work on it.

---
+++ source/src/jsx/pages/BrandingPage.jsx
@@ -0,0 +1,5 @@
+export default function BrandingPage() {
+    return (
+        <div>branding page</div>
+    )
+}
---
+++ source/src/jsx/pages/DonatePage.jsx
@@ -0,0 +1,5 @@
+export default function DonatePage() {
+    return (
+        <div>donate page</div>
+    )
+}
