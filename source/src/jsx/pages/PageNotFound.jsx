3c23e4950feae2e8eb17e890ee73f48f2e1c6017
max <70626784+notmax6677@users.noreply.github.com>
2022-08-20 12:59:02 +0300

    Revert
    Reverted project to the previous version before external changes

---
+++ source/src/jsx/pages/PageNotFound.jsx
@@ -0,0 +1,44 @@
+import { NavLink } from "solid-app-router"
+import Footer from "../components/Footer"
+
+// metadata
+import { Title } from "@solidjs/meta"
+
+// css
+import "../../css/PageNotFound/PageNotFound.css"
+import "../../css/general.css"
+
+// title string
+var pageTitle = "Rolling Rhino Remix | 404";
+
+export default function PageNotFound() {
+    return (
+        <div id="error404">
+            {/* metadata */}
+            <Title>{pageTitle}</Title>
+
+            {/* spacer */}
+            <div class="large-spacer"></div>
+
+            {/* header */}
+            <h1 class="gradient">Error 404: Page Not found</h1>
+
+            {/* paragraph */}
+            <p>
+                Are you sure you've entered the correct address?
+
+                <br></br>
+                <br></br>
+
+                {/* click to return to home */}
+                <NavLink class="paragraph-link" href="/">Click to return to Home.</NavLink>
+            </p>
+
+            {/* spacer */}
+            <div class="spacer"></div>
+
+            {/* footer */}
+            <Footer></Footer>
+        </div>
+    )
+}
